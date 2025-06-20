4b66841ac73be1ca9c7e1bb1bd221f30f848c109
Tim <timbomay@gmail.com>
2025-06-20 23:22:13 +1000

    updated forms app

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -30,23 +30,71 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnLaunch = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(259, 70);
+            this.button1.Location = new System.Drawing.Point(92, 59);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(159, 97);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = ".net log";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 97);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "VB Log";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(92, 176);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 97);
+            this.button3.TabIndex = 2;
+            this.button3.Text = ".net exception";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(299, 176);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 97);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "VB error handler";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnLaunch
+            // 
+            this.btnLaunch.Location = new System.Drawing.Point(662, 26);
+            this.btnLaunch.Name = "btnLaunch";
+            this.btnLaunch.Size = new System.Drawing.Size(97, 88);
+            this.btnLaunch.TabIndex = 4;
+            this.btnLaunch.Text = "Launch";
+            this.btnLaunch.UseVisualStyleBackColor = true;
+            this.btnLaunch.Click += new System.EventHandler(this.btnLaunch_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLaunch);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +105,10 @@ namespace WindowsFormsApp1
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnLaunch;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,31 +1,107 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MyCompany.Logging;
 using MyCompany.Logging.Abstractions;
-using MyCompany.Logging.NLogProvider;
+using MyCompany.Logging.ComBridge;
+using Scripting;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
         private static ILogger logger;
+        private static ILoggingComBridge comLogger;
         
         public Form1()
         {
             logger = LogManager.GetCurrentClassLogger();
+            comLogger = CreateCOMLogger();
             logger.Info("this is my message");
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            logger.Info("button clicked");
+            logger.Info(".Net vutton clicked");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var vbProperties = new Dictionary();
+            vbProperties.Add("aStringKey", "hello world");
+            vbProperties.Add("anIntegerKey", 12345);
+            vbProperties.Add("aBooleanKey", true);
+            comLogger.Info("avb6Codefile", "MyMethod_click", "Hello from A COM Client", vbProperties);
+        }
+
+        private LoggingComBridge CreateCOMLogger()
+        {
+            var bridge = new LoggingComBridge();
+            return bridge;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                throw new Exception("an exception Happened.");
+            }
+            catch ( Exception ex)
+            {
+                logger.Error(ex, "Something went Wrong.  {ex}", ex);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            comLogger.Error("MyVB6CodeFile.bas", "MyMethod1", "there was an error");
+        }
+
+        private void btnLaunch_Click(object sender, EventArgs e)
+        {
+            LaunchNewVersion();
+        }
+
+        /// <summary>
+        /// Launches a new instance of this application from a specified path (or the same path).
+        /// </summary>
+        /// <param name="newExecutablePath">Optional path to the new version. If null, relaunches the same executable.</param>
+        /// <param name="exitCurrent">Whether to shut down the current process after launching the new one.</param>
+        public static void LaunchNewVersion(string newExecutablePath = null, bool exitCurrent = false)
+        {
+            try
+            {
+                string currentExePath = Assembly.GetExecutingAssembly().Location;
+                string launchPath = newExecutablePath ?? currentExePath;
+
+                if (!System.IO.File.Exists(launchPath))
+                {
+                    throw new FileNotFoundException("Target executable not found", launchPath);
+                }
+
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = launchPath,
+                    UseShellExecute = true // inherits same elevation (UAC), etc.
+                };
+
+                Process.Start(startInfo);
+
+                if (exitCurrent)
+                {
+                    Environment.Exit(0); // or Application.Exit() for WinForms
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log or show error message
+                logger.Error("Failed to launch new version: ", ex);
+                throw;
+            }
         }
     }
 }
+
