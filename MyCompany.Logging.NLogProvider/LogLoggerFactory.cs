26af855330ab8a240f6d20199272c9d4d8ec9671
Tim <timbomay@gmail.com>
2025-06-25 19:05:42 +1000

    attempt to fix assemnbly laod

--- MyCompany.Logging.NLogProvider/LogLoggerFactory.cs
+++ MyCompany.Logging.NLogProvider/LogLoggerFactory.cs
@@ -1,5 +1,6 @@
 ﻿using MyCompany.Logging.Abstractions;
-using System;
+using System.IO;
+using System.Reflection;
 
 namespace MyCompany.Logging.NLogProvider
 {
@@ -12,22 +13,17 @@ namespace MyCompany.Logging.NLogProvider
         /// <summary>
         /// Initializes a new instance of the NLogLoggerFactory class.
         /// It hooks into the LogManager to provide a way to push context properties
-        /// into NLog's global context. It also validates that NLog is configured.
+        /// into NLog's global context and robustly loads NLog extensions.
         /// </summary>
         public NLogLoggerFactory()
         {
-            // Fail-fast: A logger factory should not be created if the underlying
-            // system is not properly configured. This also enables testing the
-            // partial-failure initialization path.
-            if (NLog.LogManager.Configuration == null)
-            {
-                throw new InvalidOperationException("NLog configuration is not loaded. The NLogLoggerFactory cannot be created.");
-            }
+            // THE FIX: Explicitly tell NLog where to find its extension assemblies.
+            // This is critical for robust dependency loading when hosted by a native
+            // application like VB6, as the default probing paths can be unreliable.
+            var providerAssembly = typeof(NLogLoggerFactory).Assembly;
+            NLog.LogManager.AddHiddenAssembly(providerAssembly);
 
             // This delegate connects the abstract LogManager to the concrete NLog implementation.
-            // We use GlobalDiagnosticsContext (GDC) because the properties we set (like service.name
-            // and session.id) are global for the entire application process lifetime.
-            // This is the modern, non-obsolete replacement for MDLC for this use case.
             LogManager.SetContextProperty = (key, value) => NLog.GlobalDiagnosticsContext.Set(key, value);
         }
 
