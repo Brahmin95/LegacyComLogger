374bf659a228d55448dfedd4dad325ffa2cb07b3
Tim <timbomay@gmail.com>
2025-06-27 22:25:29 +1000

    working vanilla logger (window sample still failing)

---
+++ VanillaLogger/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VanillaNLogTest
+{
+    class Program
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logging with the NLog v4 compatible ecosystem...");
+            Logger.Info("This will be ECS JSON from NLog v4.");
+            Console.WriteLine("Log message sent. Check console for JSON output.");
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
